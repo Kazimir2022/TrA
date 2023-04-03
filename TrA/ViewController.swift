50404096ba579d12d44ab3db62290a66d7578412
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-03 14:14:48 +0300

    add IBActio

--- TrA/ViewController.swift
+++ TrA/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func editDataWithDelegate(_ sender: UIButton) {
+        // получаем вью контроллер
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let editScreen = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
 
+        // передаем данные
+        editScreen.updatingData = dataLabel.text ?? ""
+        
+
+        // открываем следующий экран
+        self.navigationController?.pushViewController(editScreen, animated: true)
+    }
+    
 }
 
